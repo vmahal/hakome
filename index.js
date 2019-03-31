e49148646d91e560c622c6bd590c3eee2b99a12b
vmahal <47036560+vmahal@users.noreply.github.com>
2019-03-31 22:57:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+
+$("button").click(function(){
+  alert("Your task is added successfully!!!");
+});
+
+$(".login").on("click",function(){
+  alert("Let's Login!!");
+});
+
+$(".signup").click(function(){
+  alert("New User??");
+});
